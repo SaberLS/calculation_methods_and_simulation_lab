607d6f298a39efbf1fa92500bb560df47facfc45
SaberLS <patryk.platek19@gmail.com>
2025-10-08 21:45:35 +0200

    feat: add main with examples of binary representations

---
+++ src/main/java/pl/wszib/metodyobliczenioweisymulacja/util/BinaryRepresentation/Main.java
@@ -0,0 +1,24 @@
+package pl.wszib.metodyobliczenioweisymulacja.util.binaryrepresentation;
+
+public class Main {
+  public static void main(String[] args) {
+    float smallestSubnormal = 1.4012984643e-45f;
+    FloatBinaryRepresentation fr = new FloatBinaryRepresentation(smallestSubnormal);
+
+    // Example single precision with the smallest smallest positive subnormal float
+    // https://en.wikipedia.org/wiki/Single-precision_floating-point_format#Notable_single-precision_cases
+    System.out.println(fr);
+    System.out.println(fr.getSign());
+    System.out.println(fr.getExponent());
+    System.out.println(fr.getFraction());
+
+    // Example double precision with the smallest double greater than one
+    // https://en.wikipedia.org/wiki/Double-precision_floating-point_format#Double-precision_examples
+    DoubleBinaryRepresentation dr = new DoubleBinaryRepresentation(1.0000000000000002220);
+
+    System.out.println(dr);
+    System.out.println(dr.getSign());
+    System.out.println(dr.getExponent());
+    System.out.println(dr.getFraction());
+  }
+}
