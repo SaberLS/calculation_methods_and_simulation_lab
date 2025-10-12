647b4eeb3a83027c04d8289e07aca52863547f01
SaberLS <patryk.platek19@gmail.com>
2025-10-12 20:22:00 +0200

    refactor(utils): use FloatingPointRepresentation in MathForm of instead of BinaryRepresentation

--- src/main/java/pl/wszib/metodyobliczenioweisymulacja/util/mathform/Main.java
+++ src/main/java/pl/wszib/metodyobliczenioweisymulacja/util/mathform/Main.java
@@ -1,16 +1,23 @@
 package pl.wszib.metodyobliczenioweisymulacja.util.mathform;
 
-import pl.wszib.metodyobliczenioweisymulacja.util.binaryrepresentation.DoubleBinaryRepresentation;
-import pl.wszib.metodyobliczenioweisymulacja.util.binaryrepresentation.FloatBinaryRepresentation;
-
 public class Main {
   public static void main(String[] args) {
+    // Example single precision with the smallest smallest positive subnormal float
+    // https://en.wikipedia.org/wiki/Single-precision_floating-point_format#Notable_single-precision_cases
+    // 0 00000000 000000000000000000000012 = 0000 000116 = 2^−126 × 2^−23 = 2^−149 ≈
+    // 1.4012984643 × 10^−45
     float smallestSubnormal = 1.4012984643e-45f;
-    FloatBinaryRepresentation fr = new FloatBinaryRepresentation(smallestSubnormal);
+    FloatRepresentation fr = new FloatRepresentation(smallestSubnormal);
     System.out.println(MathForm.of(fr));
 
+    // Example smallest positive subnormal double precision number
+    // https://en.wikipedia.org/wiki/Double-precision_floating-point_format#Double-precision_examples
+    // 0 00000000000 00000000000000000000000000000000000000000000000000012
+    // ≙ 0000 0000 0000 000116
+    // ≙ +2^−1022 × 2^−52
+    // = 2^−1074
     double smallestpositiveSubnormal = 4.9406564584124654e-324;
-    DoubleBinaryRepresentation dr = new DoubleBinaryRepresentation(smallestpositiveSubnormal);
+    DoubleRepresentation dr = new DoubleRepresentation(smallestpositiveSubnormal);
     System.out.println(MathForm.of(dr));
   }
 }
--- src/main/java/pl/wszib/metodyobliczenioweisymulacja/util/mathform/MathForm.java
+++ src/main/java/pl/wszib/metodyobliczenioweisymulacja/util/mathform/MathForm.java
@@ -1,45 +1,37 @@
 package pl.wszib.metodyobliczenioweisymulacja.util.mathform;
 
-import pl.wszib.metodyobliczenioweisymulacja.util.binaryrepresentation.DoubleBinaryRepresentation;
-import pl.wszib.metodyobliczenioweisymulacja.util.binaryrepresentation.FloatBinaryRepresentation;
-
 public class MathForm {
-  public static String of(DoubleBinaryRepresentation binaryRepresentation) {
-    int bias = 1023;
-    int expValue = Integer.parseInt(binaryRepresentation.getExponent(), 2);
-    String signBit = binaryRepresentation.getSign();
-    String fraction = binaryRepresentation.getFraction();
 
-    String signSymbol = signBit.equals("0") ? "+" : "-";
-
-    // Handle special cases
-    if (expValue == 2047) {
-      if (fraction.contains("1"))
-        return signSymbol + " NaN";
-      else
-        return signSymbol + " Infinity";
-    }
+  public static String of(FloatingPointRepresentation<?> fp) {
+    int bias = fp.getBias();
+    int expValue = fp.getExponentBits();
+    long fractionBits = fp.getFractionBits();
+    String signSymbol = fp.getSignBit() == 0 ? "+" : "-";
 
     // Determine whether normalized or subnormal
     boolean isSubnormal = (expValue == 0);
     int realExp = isSubnormal ? 1 - bias : expValue - bias;
 
-    // Build conceptual mantissa representation
-    StringBuilder mantissaExpr = new StringBuilder();
-
-    if (isSubnormal) {
-      // Subnormal no implicit leading 1
-      // Value = (+1) * (0 + sum of fraction bits * 2^(-bit_index)) * 2^(1 - bias)
-      mantissaExpr.append("0");
-    } else {
-      mantissaExpr.append("1");
+    // Handle special cases: Infinity or NaN
+    if (isSpecialCase(fp, expValue, fractionBits)) {
+      if (isNaN(expValue, fractionBits, fp)) {
+        return signSymbol + " NaN";
+      } else {
+        return signSymbol + " Infinity";
+      }
     }
 
-    // Find all bits that are 1 and represent them as powers of 2
-    boolean hasFraction = false;
+    // Build conceptual mantissa representation
+    StringBuilder mantissaExpr = new StringBuilder();
+    mantissaExpr.append(isSubnormal ? "0" : "1");
+
+    // Represent fraction bits as powers of 2
     StringBuilder fractionExpr = new StringBuilder();
-    for (int i = 0; i < fraction.length(); i++) {
-      if (fraction.charAt(i) == '1') {
+    boolean hasFraction = false;
+    int precision = fp.getFractionSize();
+
+    for (int i = 0; i < precision; i++) {
+      if (((fractionBits >>> (precision - 1 - i)) & 1) == 1) {
         if (!hasFraction) {
           fractionExpr.append("2^-").append(i + 1);
           hasFraction = true;
@@ -56,54 +48,13 @@ public class MathForm {
     return String.format("(%s1) * (%s) * 2^%d", signSymbol, mantissaExpr, realExp);
   }
 
-  public static String of(FloatBinaryRepresentation binaryRepresentation) {
-    int bias = 127; // bias for single-precision float
-    int expValue = Integer.parseInt(binaryRepresentation.getExponent(), 2);
-    String signBit = binaryRepresentation.getSign();
-    String fraction = binaryRepresentation.getFraction();
+  private static boolean isSpecialCase(FloatingPointRepresentation<?> fp, int expValue, long fractionBits) {
+    int maxExp = (1 << fp.getExponentSize()) - 1;
+    return expValue == maxExp;
+  }
 
-    String signSymbol = signBit.equals("0") ? "+" : "-";
-
-    // Handle special cases
-    if (expValue == 255) {
-      if (fraction.contains("1"))
-        return signSymbol + " NaN";
-      else
-        return signSymbol + " Infinity";
-    }
-
-    // Determine whether normalized or subnormal
-    boolean isSubnormal = (expValue == 0);
-    int realExp = isSubnormal ? 1 - bias : expValue - bias;
-
-    // Build conceptual mantissa representation
-    StringBuilder mantissaExpr = new StringBuilder();
-
-    if (isSubnormal) {
-      // Subnormal no implicit leading 1
-      mantissaExpr.append("0");
-    } else {
-      mantissaExpr.append("1");
-    }
-
-    // Find all bits that are 1 and represent them as powers of 2
-    boolean hasFraction = false;
-    StringBuilder fractionExpr = new StringBuilder();
-    for (int i = 0; i < fraction.length(); i++) {
-      if (fraction.charAt(i) == '1') {
-        if (!hasFraction) {
-          fractionExpr.append("2^-").append(i + 1);
-          hasFraction = true;
-        } else {
-          fractionExpr.append(" + 2^-").append(i + 1);
-        }
-      }
-    }
-
-    if (fractionExpr.length() > 0) {
-      mantissaExpr.append(" + ").append(fractionExpr);
-    }
-
-    return String.format("(%s1) * (%s) * 2^%d", signSymbol, mantissaExpr, realExp);
+  private static boolean isNaN(int expValue, long fractionBits, FloatingPointRepresentation<?> fp) {
+    int maxExp = (1 << fp.getExponentSize()) - 1;
+    return expValue == maxExp && fractionBits != 0;
   }
 }
