a58384d26851d767b0c4bac704c8179c53eba58f
SaberLS <patryk.platek19@gmail.com>
2025-10-14 00:08:11 +0200

    feat(task-3): find machine epsilon for double

--- src/main/java/pl/wszib/metodyobliczenioweisymulacja/lab1/zadanie2/EPSILON.java
+++ src/main/java/pl/wszib/metodyobliczenioweisymulacja/lab1/zadanie2/EPSILON.java
@@ -2,8 +2,10 @@ package pl.wszib.metodyobliczenioweisymulacja.lab1.zadanie2;
 
 final class EPSILON {
     public static final float FLOAT;
+    public static final double DOUBLE;
 
     static {
+        DOUBLE = findMachineEpsilonDouble();
         FLOAT = findMachineEpsilonFloat();
     };
 
@@ -12,6 +14,16 @@ final class EPSILON {
 
         while (Float.compare((1.0f + epsilon / 2.0f), 1.0f) > 0) {
             epsilon = epsilon / 2.0f;
+        }
+
+        return epsilon;
+    }
+
+    private static double findMachineEpsilonDouble() {
+        double epsilon = 1.0;
+
+        while (Double.compare(1.0 + epsilon / 2.0, 1.0) > 0) {
+            epsilon = epsilon / 2.0;
         }
 
         return epsilon;
