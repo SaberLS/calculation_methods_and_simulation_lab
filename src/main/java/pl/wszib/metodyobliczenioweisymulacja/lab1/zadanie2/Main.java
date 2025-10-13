2f448cce9bcadedf0d14270d1e477c8135878a58
SaberLS <patryk.platek19@gmail.com>
2025-10-14 00:29:38 +0200

    feat(task-3): run epsilon search example

---
+++ src/main/java/pl/wszib/metodyobliczenioweisymulacja/lab1/zadanie2/Main.java
@@ -0,0 +1,8 @@
+package pl.wszib.metodyobliczenioweisymulacja.lab1.zadanie2;
+
+public class Main {
+  public static void main(String[] args) {
+    EPSILON.findMachineEpsilonDouble("double_epsilon.csv");
+    EPSILON.findMachineEpsilonFloat("float_epsilon.csv");
+  }
+}
